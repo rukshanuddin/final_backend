282518452c14ede3ff0dfe5026d2b16aa678f8cb
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-27 17:36:05 -0400

    tentative routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resources :topics
+      resources :links, except: :index do
+        resources :comments, only: [:create, :edit, :update, :destroy]
+      end
+    end
+  end
+
+  post '/login', to: "sessions#create"
 end
