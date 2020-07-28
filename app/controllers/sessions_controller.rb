2787af81e0a00f5ff578ff2b3660a0793b5545f7
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-27 21:11:42 -0400

    set up sessions and application controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+
+class SessionsController < ApplicationController
+  skip_before_action :authenticate
+  def create
+    user = User.find_by(email: auth_params[:email])
+    if user.authenticate(auth_params[:password])
+      jwt = Auth.issue({user_id: user.id})
+      render json: {jwt: jwt}
+    else
+      render json: {error: 'Wrong email or password'}, status: 401
+    end
+  end
+
+  private
+
+  def auth_params
+    params.require(:auth).permit(:email, :password)
+  end
+end
